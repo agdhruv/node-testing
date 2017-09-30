93e483219b81f23aab364475c78c11a695ea0f21
agdhruv <dhruv_agarwal@live.com>
2017-10-01 03:54:20 +0530

    Testing express apps using supertest

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+	res.status(404).send({
+		error: 'Page not found.',
+		name: 'Todo app v1.0'
+	});
+});
+
+app.get('/users', (req, res) => {
+	res.status(200).send([{
+		name: 'Dhruv',
+		age: 21
+	}, {
+		name: 'Ojas',
+		age: 19
+	}, {
+		name: 'Sinha',
+		age: 20
+	}]);
+});
+
+app.listen(3000);
+
+module.exports.app = app;
