9498f23c508bb4d5276259a1610483f48587916e
agdhruv <dhruv_agarwal@live.com>
2017-10-03 04:15:31 +0530

    More of testing - describeblocks for express apps

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,34 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return Hello World response', (done) => {
-	request(app)
-		.get('/')
-		.expect(404)
-		.expect((res) => {
-			expect(res.body).toInclude({
-				error: 'Page not found.'
-			});
-		})
-		.end(done);
-});
+describe('Server', () => {
 
-it('should return my user object', (done) => {
-	request(app)
-		.get('/users')
-		.expect(200)
-		.expect((res) => {
-			expect(res.body).toInclude({
-				name: 'Dhruv',
-				age: 19
-			});
-		})
-		.end(done);
+	describe('GET /', () => {
+		it('should return Hello World response', (done) => {
+			request(app)
+				.get('/')
+				.expect(404)
+				.expect((res) => {
+					expect(res.body).toInclude({
+						error: 'Page not found.'
+					});
+				})
+				.end(done);
+		});
+	});
+
+	describe('GET /users', () => {
+		it('should return my user object', (done) => {
+			request(app)
+				.get('/users')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).toInclude({
+						name: 'Dhruv',
+						age: 19
+					});
+				})
+				.end(done);
+		});
+	});
 });
